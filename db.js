17be3c984982591a2f139a98c4632427507184b8
Jessica Valim <jessicavalim94@gmail.com>
2026-06-18 20:31:08 -0300

    config banco de dados

---
+++ db.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2/promise');
+const database =mysql.createPool({
+    host: 'localhost',
+   port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'plataforma_streaming'
+}
+)
+module.exports = database;
